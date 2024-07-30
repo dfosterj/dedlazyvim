8ac265c9a41f43b093812419e9d6f9c9eee1c8b4
dfosterj <dfosterj@pm.me>
2024-07-30 12:23:58 -0500

    working gh cmds breakout toggleterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,6 +23,7 @@ map('n', '<leader>f', '<cmd>Telescope find_files<CR>', { noremap = true, silent 
 
 
 
--- github control cmds
-vim.api.nvim_set_keymap("", "<leader>prc", ":ToggleTerm gh pr create<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "<leader>prr", ":ToggleTerm gh dash pr<CR>", { noremap = true, silent = true })
+-- gh control
+vim.api.nvim_set_keymap("", '<leader>prc', ':lua require("toggleterm.terminal").Terminal:new({ cmd = "gh pr create", hidden = true }):toggle()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", '<leader>prr', ':lua require("toggleterm.terminal").Terminal:new({ cmd = "gh dash", hidden = true }):toggle()<CR>', { noremap = true, silent = true })
+
