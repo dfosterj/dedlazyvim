e5c8dc2f488fb58f7c367a9f47bbb3f4ef1833aa
dfosterj <dfosterj@pm.me>
2024-07-30 13:02:30 -0500

    full working toggleterm

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("toggleterm").setup()
 
 local gdproject = io.open(vim.fn.getcwd()..'/project.godot', 'r')
 if gdproject then
