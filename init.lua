1dc24aaad90917725f4823393ee0418b9167b7e7
dfosterj <dfosterj@pm.me>
2024-07-30 22:35:26 -0500

    rm toggleterm

--- init.lua
+++ init.lua
@@ -5,8 +5,6 @@ local gdproject = io.open(vim.fn.getcwd()..'/project.godot', 'r')
 if gdproject then
   io.close(gdproject)
   vim.fn.serverstart '/tmp/godot.pipe'
-else
-    require("toggleterm").setup()
 end
 
 local nvim_lsp = require('lspconfig')
