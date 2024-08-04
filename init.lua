84dab6798553646829afd710815abc2bd4489ea2
dfosterj <dfosterj@hotmail.com>
2024-08-03 20:51:09 -0500

    always toggleterm

--- init.lua
+++ init.lua
@@ -1,12 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("toggleterm").setup()
 
 local gdproject = io.open(vim.fn.getcwd()..'/project.godot', 'r')
 if gdproject then
   io.close(gdproject)
   vim.fn.serverstart '/tmp/godot.pipe'
-else
-    require("toggleterm").setup()
 end
 
 local nvim_lsp = require('lspconfig')
