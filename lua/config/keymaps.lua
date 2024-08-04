36a0bd4ca07da89fd02f3d7582952010cfe179d1
dfosterj <dfosterj@hotmail.com>
2024-08-03 20:45:07 -0500

    blah

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,7 +6,7 @@
 -- Disable ESLint LSP server and hide virtual text in Neovim
 -- Add this to your init.lua or init.vim file
 local isLspDiagnosticsVisible = true
-vim.keymap.set("n", "<leader>lx", function()
+vim.keymap.set("n", "<leader>xx", function()
     isLspDiagnosticsVisible = not isLspDiagnosticsVisible
     vim.diagnostic.config({
             virtual_text = isLspDiagnosticsVisible,
