0275f3242b88ff8c38f2eee3de9620a093c31770
dfosterj <dfosterj@hotmail.com>
2024-08-03 20:51:39 -0500

    space fine for find

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+-- In your plugins configuration file
+return {
+  -- Include any plugins you're using
+  {
+    'nvim-telescope/telescope.nvim',
+    config = function()
+      -- Set keybind for file search to <space>f
+      vim.api.nvim_set_keymap('n', '<space>f', ':Telescope find_files<CR>', { noremap = true, silent = true })
+    end
+  },
+  -- Your other plugins...
+}
+
