b3fd537ee316c7be1389d137cfee53ba47a6284e
dfosterj <dfosterj@hotmail.com>
2024-08-24 21:41:33 -0500

    readd toggleterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,3 +20,12 @@ vim.api.nvim_set_keymap("", "<leader>mm", ":terminal make<CR>", { noremap = true
 local map = vim.api.nvim_set_keymap
 vim.api.nvim_del_keymap('n', '<leader><leader>')
 map('n', '<C-p>', ':Telescope find_files<CR>', { noremap = true, silent = true })
+
+-- gh control
+vim.api.nvim_set_keymap("", '<leader>prc', ':lua require("toggleterm.terminal").Terminal:new({ cmd = "gh pr create", hidden = true }):toggle()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "",
+  '<leader>prr',
+  ':lua require("toggleterm.terminal").Terminal:new({ cmd = "gh dash", hidden = true, direction = "float" }):toggle()<CR>',
+  { noremap = true, silent = true }
+)
