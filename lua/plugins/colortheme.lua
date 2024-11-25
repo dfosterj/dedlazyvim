34439c72b8f334a0d649f94f9b8894370bd304a5
dfosterj <dfosterj@hotmail.com>
2024-11-24 22:53:54 -0600

    nordic

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -12,26 +12,26 @@ return {
   { "Mofiqul/dracula.nvim" },
   { "AlexvZyl/nordic.nvim" },
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "cyberdream",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "cyberdream",
+  --   },
+  -- },
+
+-- override bg
+{
+    "Mofiqul/dracula.nvim",
+    config = function()
+        require('nordic').setup({
+                -- You can set any specific options for the nordic theme here
+            })
+        -- Set the colorscheme
+        vim.cmd("colorscheme nordic")
+
+        -- Override the background color
+        vim.api.nvim_set_hl(0, "Normal", { bg = "#141414" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#141414" })
+    end,
+},
 }
-
--- example bg overwrite
--- {
---     "Mofiqul/dracula.nvim",
---     config = function()
---         require('dracula').setup({
---                 -- You can set any specific options for the Dracula theme here
---             })
---         -- Set the colorscheme
---         vim.cmd("colorscheme dracula")
-
---         -- Override the background color
---         vim.api.nvim_set_hl(0, "Normal", { bg = "#141414" })
---         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#141414" })
---     end,
--- },
