dd8a49908209c8826c2500d0fddbbbb3daf27c9e
dfosterj <dfosterj@hotmail.com>
2024-11-27 11:59:22 -0600

    simple copilot

--- lua/plugins/codepilot.lua
+++ lua/plugins/codepilot.lua
@@ -3,21 +3,4 @@ return {
 	-- 'Exafunction/codeium.vim',
 	'github/copilot.vim',
 	event = 'BufEnter'
-
-	require('copilot').setup({
-			-- General Copilot configuration options can go here
-			suggestion = {
-				-- Set the auto-accept suggestion behavior to use the Tab key
-				auto_accept = true,  -- Automatically accept a suggestion
-				keymap = {
-					accept = "<Tab>",  -- Use Tab to accept the suggestion
-				},
-			},
-			panel = {
-				-- Configuration for the suggestion panel
-				enabled = true,
-				auto_refresh = true,
-			},
-		})
-
-
+}
