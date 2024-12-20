1a861bb08ab1aa1321580b56d305badfc63b4971
dfosterj <dfosterj@hotmail.com>
2024-12-20 16:03:28 -0600

    fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,19 @@
--- In your plugins configuration file
 return {
   -- Include any plugins you're using
   {
     'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },  -- Ensure plenary.nvim is installed for Telescope
     config = function()
       -- Set keybind for file search to <space>f
       vim.api.nvim_set_keymap('n', '<space>f', ':Telescope find_files<CR>', { noremap = true, silent = true })
+
+      -- Configure Telescope settings
+      require('telescope').setup {
+        defaults = {
+          file_ignore_patterns = { "target" },  -- Ignore the target directory during searches
+        },
+      }
     end
-  },
-  require('telescope').setup {
-    defaults = {
-	    file_ignore_patterns = { "target" },
-		}
-	}
+  }
 }
 
