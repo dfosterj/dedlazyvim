a1df13b6cd8ae71c8192fd835b33ecc4ad20f6fa
dfosterj <dfosterj@hotmail.com>
2024-12-30 14:16:04 -0600

    more

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -26,32 +26,42 @@ return {
     luasnip.filetype_extend("puppet", { "pp" })
     luasnip.filetype_extend("terraform", { "tf" })
 
-    -- Keybinding to open the LuaSnip snippets directory
-    -- vim.api.nvim_set_keymap(
+    -- vim.keymap.set(
     --   "n",
-    --   "<Leader>u",
-    --   ":e " .. snippets_dir .. "<CR>",
-    --   { noremap = true, silent = true, desc = "Edit LuaSnip snippets" }
+    --   ",se",
+    --   function()
+    --     local ext = vim.fn.expand("%:e") -- Get the file extension
+    --     local snippets_dir = "~/.config/nvim/snippets/" -- Replace with your snippets directory
+    --     local snippet_file = snippets_dir .. ext .. ".lua"
+    --     vim.cmd("e " .. snippet_file) -- Open the corresponding Lua snippet file
+    --   end,
+    --   { noremap = true, silent = true, desc = "Edit LuaSnip snippets for file type" }
     -- )
-    vim.keymap.set(
-      "n",
-      ",se",
-      function()
-        local ext = vim.fn.expand("%:e") -- Get the file extension
-        local snippets_dir = "/path/to/snippets/" -- Replace with your snippets directory
-        local snippet_file = snippets_dir .. ext .. ".lua"
-        vim.cmd("e " .. snippet_file) -- Open the corresponding Lua snippet file
-      end,
-      { noremap = true, silent = true, desc = "Edit LuaSnip snippets for file type" }
-    )
 
     -- vim.api.nvim_set_keymap(
     --   "n",
-    --   "<Leader>uu",
+    --   ",se",
     --   [[:lua local ext = vim.fn.expand("%:e"); local snippets_dir = "/path/to/snippets/"; vim.cmd("e " .. snippets_dir .. ext .. ".lua")<CR>]],
     --   { noremap = true, silent = true, desc = "Edit LuaSnip snippets for file type" }
     -- )
 
+	vim.keymap.set(
+	  "n",
+	  ",se",
+	  function()
+	    local ext = vim.fn.expand("%:e") -- Get the file extension
+	    local home_dir = vim.fn.expand("~")  -- Get the user's home directory
+	    local snippets_dir = home_dir .. "/.config/nvim/snippets/"  -- Construct the path to the snippets directory
+	    local snippet_file = snippets_dir .. ext .. ".lua"  -- Define the corresponding Lua snippet file path
+	    -- Check if the snippet file exists before opening it
+	    if vim.fn.filereadable(snippet_file) == 1 then
+	      vim.cmd("e " .. snippet_file) -- Open the corresponding Lua snippet file
+	    else
+	      print("Snippet file not found: " .. snippet_file)  -- Print an error if the file doesn't exist
+	    end
+	  end,
+	  { noremap = true, silent = true, desc = "Edit LuaSnip snippets for file type" }
+	)
 
     print("LuaSnip configured with custom snippets from " .. snippets_dir)
   end,
